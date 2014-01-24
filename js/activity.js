79f4e4ed730c71cf42bfa0419f109718fb598fcb
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-01-24 11:29:37 -0300

    Add CreateJS example

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,21 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
+    var createjs = require("easel");
+    var Tween = require("tween");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
 
         // Initialize the activity.
         activity.setup();
+
+        //Draw a square on screen.
+        var canvas = document.getElementById('game');
+        var stage = new createjs.Stage(canvas);
+        var shape = new createjs.Shape();
+        shape.graphics.beginFill('rgba(255,0,0,1)').drawRoundRect(0, 0, 120, 120, 10);
+        stage.addChild(shape);
+        stage.update();
 
     });
 
--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,18 @@
 requirejs.config({
     baseUrl: "lib",
+    shim: {
+        easel: {
+            exports: 'createjs'
+        },
+        tween: {
+            deps: ['easel'],
+            exports: 'Tween'
+        }
+    },
     paths: {
-        activity: "../js"
+        activity: "../js",
+        easel: 'easeljs',
+        tween: 'tweenjs'
     }
 });
 
